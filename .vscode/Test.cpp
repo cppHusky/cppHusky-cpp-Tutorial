dba45be974088962515f811c0cc3951f1f6083c8
cppHusky <cpphusky@outlook.com>
2024-02-02 08:51:20 +0800

    Updated to Chapter 13, Section 1

--- .vscode/Test.cpp
+++ .vscode/Test.cpp
@@ -2,20 +2,17 @@
 #include<exception>
 #include<stdexcept>
 #include<vector>
-class Complex {
-    double real;
-    double image;
-public:
-    Complex(double r=0,double i=0):real{r},image{i}{}
-    Complex operator+(const Complex &c)const{
-        return Complex(real+c.real,image+c.image);
-    }
-    // friend Complex operator+(const Complex &c1, const Complex &c2){
-    //     return Complex(c1.real+c2.real,c1.image+c2.image);
-    // }
-};
+#include<sstream>
+#include<stdio.h>
 int main() {
-    Complex a(1,2);
-    2+a;
+    std::istringstream sin {"25 1.3 Alan Turing"}; //需要包含sstream库
+    std::cin.rdbuf(sin.rdbuf()); //重定向之后，cin的源就是sin中存的字符串
+    int i;
+    double d;
+    char str[50];
+    std::cin >> i >> d >> str;
+    std::cout << i << '\n' << d << '\n' << str << '\n';
+    std::cin >> str;
+    std::cout << str << std::endl;
     return 0;
 }
