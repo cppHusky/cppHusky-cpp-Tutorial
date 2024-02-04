568bda77b46f9f39fa5ceae091cb8e2acd0950d5
cppHusky <cpphusky@outlook.com>
2024-02-04 22:54:33 +0800

    Reviewed Chapter 3

--- .vscode/Test.cpp
+++ .vscode/Test.cpp
@@ -1,6 +1,12 @@
 #include <iostream> 
 #include <limits>
 int main() {
-    std::cout<<std::numeric_limits<long double>::min();
+    using namespace std;
+    for (int i = 0; i < 5; i++) { //i++与++i在这里效果相同
+        for (int j = 1; j <= 5; j++) {
+            cout << i * 5 + j << ' '; //每输出一个数字，以空格隔开
+        }
+        cout << endl; //每输出五个数字，以换行隔开
+    }
     return 0;
 }
