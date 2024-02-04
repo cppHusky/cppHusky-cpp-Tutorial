513e7c9c164ad3987c5715837e6e0368505b8f89
cppHusky <cpphusky@outlook.com>
2024-02-04 12:24:36 +0800

    Reviewed Chapter 1

--- .vscode/Test.cpp
+++ .vscode/Test.cpp
@@ -1,30 +1,16 @@
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-#include <stack>
-namespace fs = std::filesystem;
-int main() {
-    try {
-        fs::path ph {fs::current_path() / ".vscode"};
-        std::ifstream fin {ph / "in.txt"};
-        std::ofstream fout {ph / "out.txt"};
-        if (!fin.is_open()) //输入文件异常检测
-            throw std::ios_base::failure {"Cannot open in.txt"};
-        if (!fout.is_open()) //输出文件异常检测
-            throw std::ios_base::failure {"Cannot open out.txt"};
-        std::stack<char> stk; //定义一个栈，方便我们存取数据
-        int ch = {fin.get()}; //定义一个ch，逐个字符地接收get()输入数据
-        while (!fin.eof()) { //如果fin还没有读到文件结尾
-            stk.push(ch); //就说明ch是有效内容，把它填入栈中
-            ch = fin.get(); //接收下一个字符输入
-        }
-        while (stk.size()){ //只要栈中还有数据，那就说明输出还没到头
-            fout << stk.top(); //用fout输出栈顶元素
-            stk.pop(); //栈顶的元素弹出
-        }
-        std::cout << "Done" << std::endl; //提示操作完成
-    } catch (const std::ios_base::failure &msg) {
-        std::cerr << "An exception has been caught:\n" << msg.what();
-    }
+//这段程序可以告诉你当前使用的C++标准和编译器都是什么
+#include <iostream> //标准输入输出头文件
+using namespace std; //使用std命名空间
+int main() { //主函数；以上都是固定格式，照搬就行
+    cout << "Current C++ Standard: " << __cplusplus << endl; //语言标准
+#if defined(__clang__) || defined(__llvm__) //通过检测宏常量判断编译器类型
+    cout << "Current C++ Compiler: Clang" << endl; //Clang编译器
+#elif defined(__GNUC__) || defined(__GNUG__)
+    cout << "Current C++ Compiler: GCC" << endl; //GCC编译器
+#elif defined(_MSC_VER)
+    cout << "Current C++ Compiler: MSVC" << endl; //MSVC编译器
+#else //如果你使用的是其它编译器，那么将会输出Others；本书不推荐读者使用其它编译器
+    cout << "Current C++ Compiler: Others" << endl;
+#endif
     return 0;
 }
