5cab7d29b159b836a615293ad28b23d8a88db3f7
cppHusky <cpphusky@outlook.com>
2024-02-05 15:31:34 +0800

    Reviewed Chapter 4

--- .vscode/Test.cpp
+++ .vscode/Test.cpp
@@ -1,12 +1,7 @@
-#include <iostream> 
+#include <iostream>
 #include <limits>
+#include <cmath>
 int main() {
-    using namespace std;
-    for (int i = 0; i < 5; i++) { //i++与++i在这里效果相同
-        for (int j = 1; j <= 5; j++) {
-            cout << i * 5 + j << ' '; //每输出一个数字，以空格隔开
-        }
-        cout << endl; //每输出五个数字，以换行隔开
-    }
+    std::cout << std::sqrt(3);
     return 0;
 }
--- code_in_book/4.1/max3.cpp
+++ code_in_book/4.1/max3.cpp
@@ -5,7 +5,7 @@ long long max3(long long, long long, long long); //声明
 int main() {
     long long a, b, c; //定义long long型变量a,b
     cin >> a >> b >> c; //输入a,b的值
-    cout << max3(a, b, c); //输出a,b,c的最大值
+    cout << max3(a, b, c); //编译器能够在前文中找到max3的声明，所以无误
     return 0;
 }
 long long max(long long a, long long b) {
@@ -19,11 +19,6 @@ long long max(long long a, long long b) {
 }
 long long max3(long long a, long long b, long long c) {
     //接收三个long long参数，给出long long返回值
-    const long long max_ab {max(a,b)};
-    if (max_ab > c) { //如果a,b的最大值大于c，返回前者
-        return max_ab;
-    }
-    else { 否则返回后者
-        return c;
-    }
+    const long long max_ab {max(a, b)}; //先调用max(a,b)求出a与b的最大值
+    return max(max_ab, c); //同样调用max求出max_ab与c的最大值
 }
